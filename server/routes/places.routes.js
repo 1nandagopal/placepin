d656a9f372fdbf607e037475a146988361aee97f
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-09 16:12:36 +0530

    Added middleware to restrict unauthorised users from mutatuing places

--- server/routes/places.routes.js
+++ server/routes/places.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const fileUpload = require("../utils/multer");
@@ -8,12 +9,27 @@ const {
   getPlacesByUserId,
   createPlace,
 } = require("../controllers/places.controllers");
+const CustomError = require("../models/customError");
 
 const placesRouter = express.Router();
 
 placesRouter.get("/", getAllPlaces);
 placesRouter.get("/:placeId", getPlaceById);
 placesRouter.get("/user/:userId", getPlacesByUserId);
+
+placesRouter.use((req, res, next) => {
+  if (req.method === "OPTIONS") return next();
+
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    req.user = { userId: decodedToken.userId };
+    next();
+  } catch (error) {
+    return next(new CustomError("Authentication failed.", 400));
+  }
+});
+
 placesRouter.post("/new", fileUpload.single("image"), createPlace);
 
 module.exports = placesRouter;
