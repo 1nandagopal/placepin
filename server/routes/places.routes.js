a40525a4dfa9f5e0f9e8153ff700c0a4345babf3
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-09 19:42:33 +0530

    Implemented catchAsync for places route handlers

--- server/routes/places.routes.js
+++ server/routes/places.routes.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const fileUpload = require("../utils/multer");
+const catchAsync = require("../utils/catchAsync");
 const {
   getAllPlaces,
   getPlaceById,
@@ -15,25 +16,23 @@ const CustomError = require("../models/customError");
 
 const placesRouter = express.Router();
 
-placesRouter.get("/", getAllPlaces);
-placesRouter.get("/:placeId", getPlaceById);
-placesRouter.get("/user/:userId", getPlacesByUserId);
+placesRouter.get("/", catchAsync(getAllPlaces));
+placesRouter.get("/:placeId", catchAsync(getPlaceById));
+placesRouter.get("/user/:userId", catchAsync(getPlacesByUserId));
 
-placesRouter.use((req, res, next) => {
-  if (req.method === "OPTIONS") return next();
+placesRouter.use(
+  catchAsync((req, res, next) => {
+    if (req.method === "OPTIONS") return next();
 
-  try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     req.user = { userId: decodedToken.userId };
     next();
-  } catch (error) {
-    return next(new CustomError("Authentication failed.", 400));
-  }
-});
+  })
+);
 
-placesRouter.post("/new", fileUpload.single("image"), createPlace);
-placesRouter.patch("/:placeId", updatePlace);
-placesRouter.delete("/:placeId", deletePlace);
+placesRouter.post("/new", fileUpload.single("image"), catchAsync(createPlace));
+placesRouter.patch("/:placeId", catchAsync(updatePlace));
+placesRouter.delete("/:placeId", catchAsync(deletePlace));
 
 module.exports = placesRouter;
