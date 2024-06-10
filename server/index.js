c75bf2e711627237d7132368e2e078ecfec4f486
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-10 09:07:51 +0530

    Minor imporvements

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const morgan = require("morgan");
 const express = require("express");
 
@@ -10,10 +12,29 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/user", userRouter);
 app.use("/api/places", placesRouter);
 
 app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      if (err) console.log(err);
+    });
+  }
+
   if (res.headerSent) return next(error);
 
   res.status(error.code || 500);
