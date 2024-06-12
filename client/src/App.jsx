46434806a4fbf4a4ada835814bcc01da85706da0
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-12 10:47:26 +0530

    Created places route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Places from "./routes/Places";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="bg-gray-900 h-screen text-white p-8 overflow-auto">
+      <Routes>
+        <Route path="/" element={<Places />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
