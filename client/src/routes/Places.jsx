5bf3e695e256bbf3e9b4d06576abe63319efa7e1
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-12 16:04:16 +0530

    Fetch places from api upon rendering.

--- client/src/routes/Places.jsx
+++ client/src/routes/Places.jsx
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import useHTTP from "../hooks/useHTTP";
 
 function Places() {
   const [places, setPlaces] = useState([]);
+  const { sendRequest, isLoading, error } = useHTTP();
+  useEffect(() => {
+    async function getAllPlaces() {
+      try {
+        const response = await sendRequest("/api/places");
+        setPlaces(response.places);
+      } catch (err) {
+        error && console.error(error);
+      }
+    }
+    getAllPlaces();
+  });
   return <div>Places</div>;
 }
 
