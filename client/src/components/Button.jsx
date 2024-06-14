e3bc605a4a0de926330370dd506e7b4cc8453679
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-14 17:01:07 +0530

    Styled custom Button component.

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 
+const colorVariants = {
+  red: "bg-red-700 hover:bg-red-800 focus:ring-red-900 disabled:bg-red-700",
+  primary:
+    "bg-primary-700 hover:bg-primary-800 focus:ring-primary-900 disabled:bg-primary-700",
+};
+
 function Button({
   children,
   type = "button",
   disabled = false,
   isLoading,
   onClick,
+  color = "primary",
+  classes = "",
 }) {
   return (
-    <button type={type} disabled={disabled || isLoading} onClick={onClick}>
+    <button
+      type={type}
+      disabled={disabled || isLoading}
+      onClick={onClick}
+      className={`text-base px-4 py-2 rounded-lg focus:ring-2 ${colorVariants[color]} ${classes}`}
+    >
       {children}
     </button>
   );
