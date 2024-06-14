fc7925c3f4db5ce73d3acf6d189e873724897256
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-14 08:39:16 +0530

    Added persistent user login using localStorage

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -27,15 +27,30 @@ function reducer(state, action) {
 
 export function AuthProvider(props) {
   const [state, dispatch] = useReducer(reducer, init);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user && user.token && new Date(user.expiresIn) > new Date()) {
+      login(user.token, user.userId);
+    } else {
+      logout();
+    }
+  }, []);
+
   const login = (
     token,
-    userId,
-    expiresIn = new Date(new Date().getTime() + 1000 * 60 * 60 * 6)
+    userId
+    // expiresIn = new Date(new Date().getTime() + 1000 * 60 * 60 * 6)
   ) => {
+    localStorage.setItem(
+      "user",
+      JSON.stringify({ token, userId, expiresIn: expiresIn.toISOString() })
+    );
     dispatch({ type: "LOGIN", token, userId });
   };
 
   const logout = () => {
+    localStorage.removeItem("user");
     dispatch({ type: "LOGOUT" });
   };
 
