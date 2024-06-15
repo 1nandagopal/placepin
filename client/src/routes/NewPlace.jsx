b2522f9f16fd58ed1c95cfff6c84dc8fc8775ebf
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-15 16:36:52 +0530

    Added handler fn for new place form submission.

--- client/src/routes/NewPlace.jsx
+++ client/src/routes/NewPlace.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import Form from "../components/Form";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import useHTTP from "../hooks/useHTTP";
+import { AuthContext } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
 
 function NewPlace() {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { sendRequest, isLoading, error } = useHTTP();
+
+  const handleFormSubmit = async (data) => {
+    try {
+      await sendRequest(
+        "/api/places/new",
+        "post",
+        { ...data, image: data.image[0] },
+        {
+          "Content-Type": "multipart/form-data",
+          Authorization: "Bearer " + auth.token,
+        }
+      );
+      navigate("/myplaces");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="mx-auto max-w-3xl text-white">
       <h2 className="my-4 text-3xl font-semibold">Add a new place</h2>
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
         <Input
           name="title"
           label="Title"
@@ -25,7 +49,8 @@ function NewPlace() {
           label="Address"
           placeholder="Enter place address here"
         />
-        <Button type="submit" isLoading={false} classes="text-base w-32">
+        <div className="text-red-400 text-sm w-full h-6">{error}</div>
+        <Button type="submit" isLoading={isLoading} classes="text-base w-32">
           Add Place
         </Button>
       </Form>
