a20e2a7a609bdf3301e9d57f454d2011f69e9926
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-15 17:27:23 +0530

    Minor fix.

--- client/src/routes/NewPlace.jsx
+++ client/src/routes/NewPlace.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Form from "../components/Form";
 import Input from "../components/Input";
 import Button from "../components/Button";
@@ -16,6 +16,10 @@ function NewPlace() {
   const auth = useContext(AuthContext);
   const navigate = useNavigate();
   const { sendRequest, isLoading, error } = useHTTP();
+
+  useEffect(() => {
+    if (!auth.isLoggedIn) navigate("/auth");
+  }, []);
 
   const handleFormSubmit = async (data) => {
     try {
@@ -35,40 +39,42 @@ function NewPlace() {
   };
 
   return (
-    <div className="mx-auto max-w-3xl text-white">
-      <h2 className="my-4 text-3xl font-semibold">Add a new place</h2>
-      <Form onSubmit={handleFormSubmit}>
-        <Input
-          name="title"
-          validators={titleValidators}
-          label="Title"
-          placeholder="Enter place title here"
-        />
-        <Input
-          name="image"
-          validators={imageValidators}
-          label="Upload image"
-          type="file"
-        />
-        <Input
-          name="description"
-          validators={descriptionValidators}
-          label="Description"
-          type="textarea"
-          placeholder="Write place description here"
-        />
-        <Input
-          name="address"
-          validators={addressValidators}
-          label="Address"
-          placeholder="Enter place address here"
-        />
-        <div className="text-red-400 text-sm w-full h-6">{error}</div>
-        <Button type="submit" isLoading={isLoading} classes="text-base w-32">
-          Add Place
-        </Button>
-      </Form>
-    </div>
+    auth.isLoggedIn && (
+      <div className="mx-auto max-w-3xl text-white">
+        <h2 className="my-4 text-3xl font-semibold">Add a new place</h2>
+        <Form onSubmit={handleFormSubmit}>
+          <Input
+            name="title"
+            validators={titleValidators}
+            label="Title"
+            placeholder="Enter place title here"
+          />
+          <Input
+            name="image"
+            validators={imageValidators}
+            label="Upload image"
+            type="file"
+          />
+          <Input
+            name="description"
+            validators={descriptionValidators}
+            label="Description"
+            type="textarea"
+            placeholder="Write place description here"
+          />
+          <Input
+            name="address"
+            validators={addressValidators}
+            label="Address"
+            placeholder="Enter place address here"
+          />
+          <div className="text-red-400 text-sm w-full h-6">{error}</div>
+          <Button type="submit" isLoading={isLoading} classes="text-base w-32">
+            Add Place
+          </Button>
+        </Form>
+      </div>
+    )
   );
 }
 
