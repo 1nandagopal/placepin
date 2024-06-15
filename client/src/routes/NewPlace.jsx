a51d8dac5d8866c4e6007a00bc76a68ffb96e065
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-15 10:57:33 +0530

    Styled NewPlace component.

--- client/src/routes/NewPlace.jsx
+++ client/src/routes/NewPlace.jsx
@@ -5,24 +5,31 @@ import Button from "../components/Button";
 
 function NewPlace() {
   return (
-    <Form>
-      <Input name="title" label="Title" placeholder="Enter place title here" />
-      <Input name="image" label="Upload image" type="file" />
-      <Input
-        name="description"
-        label="Description"
-        type="textarea"
-        placeholder="Write place description here"
-      />
-      <Input
-        name="address"
-        label="Address"
-        placeholder="Enter place address here"
-      />
-      <Button type="submit" isLoading={false} classes="text-base w-32">
-        Add Place
-      </Button>
-    </Form>
+    <div className="mx-auto max-w-3xl text-white">
+      <h2 className="my-4 text-3xl font-semibold">Add a new place</h2>
+      <Form>
+        <Input
+          name="title"
+          label="Title"
+          placeholder="Enter place title here"
+        />
+        <Input name="image" label="Upload image" type="file" />
+        <Input
+          name="description"
+          label="Description"
+          type="textarea"
+          placeholder="Write place description here"
+        />
+        <Input
+          name="address"
+          label="Address"
+          placeholder="Enter place address here"
+        />
+        <Button type="submit" isLoading={false} classes="text-base w-32">
+          Add Place
+        </Button>
+      </Form>
+    </div>
   );
 }
 
