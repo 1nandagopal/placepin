39e1b4e90a5a0bf4407c5f3c187abedc430eab44
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-16 11:43:27 +0530

    Added functionality to fetch and render places in MyPlace.

--- client/src/components/PlaceList.jsx
+++ client/src/components/PlaceList.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import PlaceItem from "./PlaceItem";
 
 function PlaceList({ places }) {
   return (
     <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4">
-      {places.map((place) => {
-        <PlaceList place={place} key={place.id} />;
-      })}
+      {places.map((place) => (
+        <PlaceItem place={place} key={place.id} />
+      ))}
     </div>
   );
 }
--- client/src/routes/MyPlaces.jsx
+++ client/src/routes/MyPlaces.jsx
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import PlaceList from "../components/PlaceList";
+import useHTTP from "../hooks/useHTTP";
+import { AuthContext } from "../context/authContext";
 
 function MyPlaces() {
   const [places, setPlaces] = useState([]);
+  const { sendRequest, error } = useHTTP();
+  const auth = useContext(AuthContext);
+
+  useEffect(() => {
+    const getPlaces = async () => {
+      try {
+        const response = await sendRequest(`/api/places/user/${auth.userId}`);
+        setPlaces(response.places);
+      } catch (err) {
+        if (err) console.error(error);
+      }
+    };
+    if (auth.isLoggedIn) getPlaces();
+  }, [auth]);
+
   return (
     <div>
       <h2 className="text-3xl my-4">My Places</h2>
