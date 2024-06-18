6843df90e77c3a06a727227b10fedd4b5a07cfbc
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-18 10:01:23 +0530

    Styled NavBar.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import Button from "./Button";
 import { Link } from "react-router-dom";
+import { FaPlus } from "react-icons/fa6";
+import { TbLocation } from "react-icons/tb";
 
 function NavBar() {
   return (
-    <nav>
-      <Link to="/">
-        <h1>PlacePin</h1>
+    <nav className="text-white flex items-center justify-between px-1 mb-5">
+      <Link to="/" className="group flex items-center">
+        <TbLocation className="text-6xl mt-1 mr-1.5 group-hover:text-fuchsia-500 transition-all duration-300" />
+        <h1 className="text-4xl font-semibold cursor-pointer bg-gradient-to-r from-blue-400 to-fuchsia-600  bg-white bg-clip-text bg-no-repeat bg-[length:0%] group-hover:bg-[length:100%] group-hover:text-transparent transition-all duration-300">
+          PlacePin
+        </h1>
       </Link>
-      <div>
+      <div className="mt-2 flex items-center space-x-5 text-sm">
         <Link to="new">
-          <Button>New Place</Button>
+          <Button classes="flex items-center">
+            <FaPlus className="mt-0.5 mr-1.5 h-4" />
+            New Place
+          </Button>
         </Link>
         <Link to="/myplaces">
           <Button>My Places</Button>
         </Link>
-        <Button>Log Out</Button>
+        <Button color="red" classes="font-medium text-lg">
+          Log Out
+        </Button>
         <Link to="/auth">
           <Button>Log In</Button>
         </Link>
