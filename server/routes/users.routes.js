e4aa5dc151cfcaa8bf6274df0dce4ce490b15e59
Nandagopal S <168453056+1nandagopal@users.noreply.github.com>
2024-06-24 17:46:07 +0530

    Added added strict field checks for users route.

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -3,10 +3,21 @@ const express = require("express");
 const catchAsync = require("../utils/catchAsync");
 const { signupUser, loginUser } = require("../controllers/users.controllers");
 const { loginValidations, signupValidations } = require("../utils/validators");
+const { checkExact } = require("express-validator");
 
 const userRouter = express.Router();
 
-userRouter.post("/signup", signupValidations, catchAsync(signupUser));
-userRouter.post("/login", loginValidations, catchAsync(loginUser));
+userRouter.post(
+  "/signup",
+  signupValidations,
+  checkExact([], { message: "Unknown fields detected" }),
+  catchAsync(signupUser)
+);
+userRouter.post(
+  "/login",
+  loginValidations,
+  checkExact([], { message: "Unknown fields detected" }),
+  catchAsync(loginUser)
+);
 
 module.exports = userRouter;
